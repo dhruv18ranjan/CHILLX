a9e26e3e3087a5f216aa1fa0a391d2dade9433a5
dhruv <dhruv18ranjan@gmail.com>
2023-05-01 22:02:11 +0530

    ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import { useEffect } from "react";
+import { fetchData } from "./utils/api"
 
 function App() {
+
+  useEffect(()=>{
+    apiTesting();
+  },[])
+
+  const apiTesting = () =>{
+    fetchData("/movie/popular").then((res)=>{
+      console.log(res);
+    })
+  }
 
   return (
     <div className='App'>
