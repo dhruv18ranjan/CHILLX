41556ff8d3d0104a86b1f2ffe41f09e5ffb8b168
dhruv <dhruv18ranjan@gmail.com>
2023-05-17 22:46:13 +0530

    navigation and header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,48 +10,69 @@ import ContentWrapper from "../contentWrapper/ContentWrapper";
 import logo from "../../assets/movix-logo.svg";
 
 const Header = () => {
-    const [show, setShow] = useState("top");
-    const [lastScrollY, setLastScrollY] = useState(0);
-    const [mobileMenu, setMobileMenu] = useState(false);
-    const [query, setQuery] = useState("");
-    const [showSearch, setShowSearch] = useState("");
-    const navigate = useNavigate();
-    const location = useLocation();
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
 
-    const openSearch=()=>{
-      setMobileMenu(false);
-      setShowSearch(true);
+  const searchQueryHandler = (e) => {
+    if (e.key === 'Enter' && query.length > 0) {
+      navigate(`/search/${query}`)
+      setTimeout(() => {
+        setShowSearch(false);
+      }, 1000);
     }
-    const openMobileMenu=()=>{
-      setMobileMenu(true);
-      setShowSearch(false);
-    }
+  }
 
-    return (
-      <header className={`header ${mobileMenu ? "mobileView" :""} ${show}`}>
-        <ContentWrapper>
-          <div className="logo">
-            <img src={logo} alt="" />
-          </div>
-          <ul className="menuItems">
-            <li className="menuItem">Movies</li>
-            <li className="menuItem">TV Shows</li>
-            <li className="menuItem"><HiOutlineSearch /> </li>
-          </ul>
+  const openSearch = () => {
+    setMobileMenu(false);
+    setShowSearch(true);
+  }
+  const openMobileMenu = () => {
+    setMobileMenu(true);
+    setShowSearch(false);
+  }
+
+  return (
+    <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem"><HiOutlineSearch /> </li>
+        </ul>
 
         <div className="mobileMenuItems">
-          <HiOutlineSearch />
+          <HiOutlineSearch onClick={openSearch} />
           {
-            mobileMenu ? (<VscChromeClose onClick={()=>setMobileMenu(false)} />): <SlMenu  onClick={openMobileMenu} />
+            mobileMenu ? (<VscChromeClose onClick={() => setMobileMenu(false)} />) : <SlMenu onClick={openMobileMenu} />
           }
-          
-          
+
+
         </div>
 
+      </ContentWrapper>
+      {showSearch && (<div className="searchBar">
+        <ContentWrapper>
+          <div className="searchInput">
+            <input type="text"
+              placeholder='search for a movie or a tv show...'
+              onChange={(e) => setQuery(e.target.value)}
+              onKeyUp={searchQueryHandler}
+            />
+            <VscChromeClose onClick={() => setShowSearch(false)} />
+          </div>
         </ContentWrapper>
-      </header>
-    );
+      </div>)}
+    </header>
+  );
 
-    };
+};
 
-    export default Header;
+export default Header;
