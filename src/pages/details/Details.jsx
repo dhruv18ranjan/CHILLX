29dfb4ba3040e3e3213f311e72f60a5a9cb692b3
dhruv <dhruv18ranjan@gmail.com>
2023-06-03 23:33:20 +0530

    detailPage init

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import "./styles.scss"
+import useFetch from '../../hooks/useFetch'
+import { useParams } from 'react-router-dom'
+import DetailsBanner from './detailsBanner/DetailsBanner'
 
 
 const Details = () => {
+
+  // const {mediaType,id} =useParams();
+  // const{data,loading} = useFetch('/${mediaType}/${id}');
+
   return (
-    <div>Details</div>
+    <div> <DetailsBanner /> </div> 
   )
 }
 
